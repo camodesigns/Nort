8d2202c54b2506b6982caab1fe7056875ef24cce
camodesigns <camilo970403@gmail.com>
2022-11-29 10:14:48 -0500

    SettingProject
    Commit Unreal project

---
+++ Source/Nort/Nort.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Nort.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Nort, "Nort" );
---
+++ Source/Nort/NortGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NortGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NORT_API ANortGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
